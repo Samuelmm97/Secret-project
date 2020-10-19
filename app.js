13fd431dcde748b459481ca69dbce518df4bec17
unknown <sammymenaged@yahoo.com>
2020-10-19 00:51:39 -0400

    updated facebook Oauth

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.get("/auth/google/secrets",
     res.redirect('/secrets');
   });
 
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ["profile"] }));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ["email"] }));
 
 app.get('/auth/facebook/secrets',
   passport.authenticate("facebook", { failureRedirect: '/login' }),
